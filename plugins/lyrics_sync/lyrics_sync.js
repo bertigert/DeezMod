513f5213550c3b55f576882612f783ddebd0a960
bertigert <112719402+bertigert@users.noreply.github.com>
2025-06-22 22:05:24 +0200

    better click feedback

--- plugins/lyrics_sync/lyrics_sync.js
+++ plugins/lyrics_sync/lyrics_sync.js
@@ -1587,7 +1587,8 @@ module.exports = {
                         background: var(--tempo-colors-background-neutral-tertiary-hovered);
                         color: var(--tempo-colors-text-neutral-primary-hovered);
                     }
-                    button.lyrics_sync_custom_lyrics.active {
+                    button.lyrics_sync_custom_lyrics.active,
+                    button.lyrics_sync_custom_lyrics:active {
                         color: var(--tempo-colors-icon-accent-primary-default);
                     }
                     button.lyrics_sync_custom_lyrics > svg {
