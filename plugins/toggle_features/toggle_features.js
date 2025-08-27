bd721a24b39ec41c5aba5c73881537532da349e7
bertigert <112719402+bertigert@users.noreply.github.com>
2025-08-27 13:18:10 +0200

    more robust hooking

--- plugins/toggle_features/toggle_features.js
+++ plugins/toggle_features/toggle_features.js
@@ -1,7 +1,7 @@
 module.exports = {
     name: "Toggle Features",
     description: "Enable or disable features which may or may not be experimental/web version only.",
-    version: "1.0.2",
+    version: "1.0.3",
     author: "bertigert",
     context: ["renderer"],
     scope: ["own"],
@@ -9,15 +9,19 @@ module.exports = {
         "use strict";
         // ======= Settings START =======
         
+        
         const LOG_ALL_FEATURES_DEBUG = true; // useful to see all features (gets logged in the (dev tools) console, use https://github.com/bertigert/DeezMod/blob/main/plugins/enable_dev_mode.js to view)
-
+        
+        
         const DEEZER_CUSTOM_FEATURES = {
             // gapless_playback: true,
             deeztools: true, // simple way to toggle most of the custom features
         }
-
+        
+        // true = enable, false = disable, null = leave as is (only some features differentiate between false and null)
         const SPECIAL_FEATURES = {
             spoof_family: false, // Spoof your account to be the head of a family plan if you are a child account of a family account, opening up more features for you. (e.g. linking to last.fm)
+            download_on_web: null, // Enable downloading on the web version of Deezer
         }
 
         // ======= Settings END =======
@@ -34,10 +38,66 @@ module.exports = {
             console.debug("[Toggle Features]", ...args);
         }
         
+        function get_user_id(resp_json) {
+            return resp_json.results.USER.USER_ID;
+        }
+
+        function patch(resp_json) {
+            // Special features
+            if (SPECIAL_FEATURES.spoof_family) {
+                resp_json.results.USER.MULTI_ACCOUNT = {"ENABLED": true,"ACTIVE": true,"CHILD_COUNT": 0,"MAX_CHILDREN": 0,"PARENT": null,"IS_KID": false,"IS_SUB_ACCOUNT": false}
+            }
+
+            if (SPECIAL_FEATURES.download_on_web) {
+                localStorage.setItem("deezTools_features_offline_"+get_user_id(resp_json), "true");
+            } else if (SPECIAL_FEATURES.download_on_web === false) {
+                localStorage.removeItem("deezTools_features_offline_"+get_user_id(resp_json));
+            }
+
+
+            // Deezer custom features
+            const features = resp_json.results.__DZR_GATEKEEPS__;
+
+            if (LOG_ALL_FEATURES_DEBUG) {
+                log('All Features:', features, "Special Features:", SPECIAL_FEATURES);
+            }
+
+            for (let feature of Object.entries(DEEZER_CUSTOM_FEATURES)) {
+                features[feature[0]] = feature[1];
+                log(feature[1] ? 'Enabled' : 'Disabled', feature[0]);
+            }
+        }
+
+        class Hooks {
+            static HOOK_INDEXES = Object.freeze({
+                FETCH: 0,
+                ALL: 1
+            });
+
+            // we use this approach to unhook to avoid unhooking hooks created after our hooks
+            static is_hooked = [true]; // start enabled since we only hook once
+
+            static toggle_hooks(enabled, ...args) {
+                for (const arg of args) {
+                    switch (arg) {
+                        case Hooks.HOOK_INDEXES.ALL:    
+                            Hooks.is_hooked.fill(enabled);
+                            return;
+                        case Hooks.HOOK_INDEXES.FETCH:
+                            Hooks.is_hooked[arg] = enabled;
+                            break;
+                    }
+                }
+            }
+        }
+
         const original_fetch = window.fetch;
 
         log("Hooking fetch");
-        window.fetch = async function (...args) {
+        
+        async function hooked_fetch(...args) {
+            if (!Hooks.is_hooked[Hooks.HOOK_INDEXES.FETCH]) return original_fetch.apply(this, args);
+
             try {
                 const url = new URL(args[0]);
             
@@ -56,28 +116,12 @@ module.exports = {
                 const resp_json = await response.json();
 
                 if (resp_json.results) {
-                    // Special features
-                    if (SPECIAL_FEATURES.spoof_family) {
-                        resp_json.results.USER.MULTI_ACCOUNT = {"ENABLED": true,"ACTIVE": true,"CHILD_COUNT": 0,"MAX_CHILDREN": 0,"PARENT": null,"IS_KID": false,"IS_SUB_ACCOUNT": false}
-                    }
-                    
-
-                    // Deezer custom features
-                    const features = resp_json.results.__DZR_GATEKEEPS__;
-
-                    if (LOG_ALL_FEATURES_DEBUG) {
-                        log('All Features:', features, "Special Features:", SPECIAL_FEATURES);
-                    }
-
-                    for (let feature of Object.entries(DEEZER_CUSTOM_FEATURES)) {
-                        features[feature[0]] = feature[1];
-                        log(feature[1] ? 'Enabled' : 'Disabled', feature[0]);
-                    }
+                    patch(resp_json);
                 }
 
                 // since this request is only made once, we can unhook now
-                log("Unhooking fetch");
-                window.fetch = original_fetch;
+                debug("Unhooking fetch");
+                Hooks.toggle_hooks(false, Hooks.HOOK_INDEXES.ALL);
 
                 return new Response(JSON.stringify(resp_json), {
                     status: response.status,
@@ -89,5 +133,15 @@ module.exports = {
                 return original_fetch.apply(window, args);
             }
         }
+
+        // only change the function which gets called, not the attributes of the original fetch function
+        Object.setPrototypeOf(hooked_fetch, original_fetch);
+        Object.getOwnPropertyNames(original_fetch).forEach(prop => {
+            try {
+                hooked_fetch[prop] = original_fetch[prop];
+            } catch (e) {}
+        });
+        window.fetch = hooked_fetch;
+        window.fetch._modified_by_toggle_features = true;
     }
 }
